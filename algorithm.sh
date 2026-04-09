c020e6805dcaad6ffc64e27a0ea282daa9a97b8b
ThanadechHub <57976945+ThanadechHub@users.noreply.github.com>
2026-04-09 23:07:19 +0700

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee value_pi_report.txt
-echo "3.14159" | tee -a value_pi_report.txt
+echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee example_report.txt
+echo "3.14159" | tee -a example_report.txt
 
 # UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI. ALSO REMOVE THE PREVIOUS ECHO STATEMENTS.
 # echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
